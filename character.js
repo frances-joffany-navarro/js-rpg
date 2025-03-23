b04c21af3e4d13b64ef139f418bbd48fc6d4bb73
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2025-03-23 01:49:46 +0100

    adding race function

--- character.js
+++ character.js
@@ -5,19 +5,46 @@ function Person(race, item, name) {
   this.name = name;
   this.currenthealth = 100;
   this.maxHealth = 100;
-  /* this.player = player; */
-  this.oldhealth = 100;
 
   this.min = 3;
   this.maxDamage = 20;
   this.maxHealing = 30;
 
   this.heal = function () {
-    return Math.floor(Math.random() * (this.maxHealing - this.min + 1)) + this.min;
+    return Math.floor(Math.random() * (this.maxHealing - this.min) + this.min);
   };
 
   this.damage = function () {
-    return Math.floor(Math.random() * (this.maxDamage - this.min + 1)) + this.min;
+    const randomDamageNumber = Math.floor(Math.random() * (this.maxDamage - this.min) + this.min);
+    let totalDamage;
+    switch (this.race) {
+      case "humans":
+        //20% less damage taken
+        totalDamage = randomDamageNumber - Math.round(randomDamageNumber * 0.20)
+        break;
+
+      case "orcs":
+        /* 40% more max health */
+        this.maxHealth += this.maxHealth * 0.4;
+        totalDamage = randomDamageNumber;
+        break;
+
+      case "elves":
+        //30% chance to deflect the attack back to the opponent. 
+        // The attacker takes damage equal to 50% of the original hit. 
+        // The elf takes no damage.
+        totalDamage = randomDamageNumber;
+        break;
+
+      case "vampires":
+        totalDamage = randomDamageNumber;
+        break;
+
+      default:
+        break;
+    }
+
+    return totalDamage;
   };
 
   this.totalDamage = this.damage();// how much damage you will give to your opponent
--- gamelogic.js
+++ gamelogic.js
@@ -1,11 +1,44 @@
 import { Person } from "./character.js";
 
 //Initialize Temporary Characters
-const player1 = new Person("humans", "boots", "Frances");
+const player1 = new Person("elves", "boots", "Frances");
 const player2 = new Person("orcs", "boots", "Computer");
 
 player1.displayChar(), player2.displayChar()
 
-function races (race){
-  
+console.log(player1.totalDamage)
+
+function race (player) {
+  switch (this.race) {
+    case "humans":
+      //20% less damage taken
+
+      break;
+
+    case "orcs":
+      /* 40% more max health */
+      this.maxHealth += this.maxHealth * 0.4;
+      totalDamage = randomDamageNumber;
+      break;
+
+    case "elves":
+      //30% chance to deflect the attack back to the opponent. 
+      // The attacker takes damage equal to 50% of the original hit. 
+      // The elf takes no damage.
+      const randomLuck = Math.floor(Math.random() * 100) + 1;
+      if (randomLuck <= 30) {
+        totalDamage = 0;
+        opponent.totalDamage
+      } else {
+        totalDamage = randomDamageNumber;
+      }
+      break;
+
+    case "vampires":
+
+      break;
+
+    default:
+      break;
+  }
 }
