30b7e003da119f2b787502fd3a36392a6fa67e33
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2025-03-24 19:56:30 +0100

    fix players turn

--- gamelogic.js
+++ gamelogic.js
@@ -7,40 +7,38 @@ const player2 = new Person("orc", "boots", "Computer");
 player1.currenthealth = 70;
 
 let turn = 0;
+let starting = 1;
 
 counter(turn);
 
 function counter(turn) {
+  let move;
   if (turn == 0) {
     console.log(`${player1.name} turn!`);
     console.log(`Current Health: ${player1.currenthealth}`);
 
-    let move = prompt("What is your move? \n 1 - Attack \n 2 - Heal \n 3 - Surrender \n Please enter the number:");
+    if (starting === 1) {
+      move = prompt("What is your move? \n 1 - Attack \n 2 - Heal \n 3 - Surrender \n Please enter the number:");
+      starting = 0;
+    } else {
+      move = prompt("Please try again! What is your move? \n 1 - Attack \n 2 - Heal \n 3 - Surrender \n Please enter the number:");
+    }
+
     console.log(`${player1.name} choose ${move}`);
     checkMove(player1, player2, move);
-    //Next Player
-    /* if (move != "3") {
-      turn = 1;
-      counter(turn);
-    } else {
-      console.log("Game Over");
-    } */
-
   } else {
     console.log(`${player2.name} turn!`);
     console.log(`Current Health: ${player2.currenthealth}`);
 
-    let move = prompt("What is your move? \n 1 - Attack \n 2 - Heal \n 3 - Surrender \n Please enter the number:");
+    if (starting === 1) {
+      move = prompt("What is your move? \n 1 - Attack \n 2 - Heal \n 3 - Surrender \n Please enter the number:");
+      starting = 0;
+    } else {
+      move = prompt("Please try again! What is your move? \n 1 - Attack \n 2 - Heal \n 3 - Surrender \n Please enter the number:");
+    }
+
     console.log(`${player2.name} choose ${move}`);
     checkMove(player2, player1, move);
-
-    /* //Next Player
-    if (move != "3") {
-      turn = 0;
-      counter(turn);
-    } else {
-      console.log("Game Over");
-    } */
 
     /* if (player2.currenthealth <= 0) {
       console.log(`Game over! ${player1.name} WON!`);
@@ -59,10 +57,9 @@ function checkMove(player, opponent, move) {
 
     case "2":
       if (player.currenthealth === player.maxHealth) {
-        console.log(`${player.name} current health is still full.`);
-        move = prompt("Please try again! What is your move? \n 1 - Attack \n 2 - Heal \n 3 - Surrender \n Please enter the number:");
-        console.log(`${player.name} choose ${move}`);
-        checkMove(player, player2, move);
+        console.log(`${player.name} current health is still full. Pick another move.`);
+        counter(turn);
+
       } else {
         let healingPower = player.heal();
         let currenthealth = 0;
@@ -79,6 +76,14 @@ function checkMove(player, opponent, move) {
           player.currenthealth = currenthealth;
         }
         console.log(`${player.name}'s current health: ${player.currenthealth}`);
+
+        if (turn === 0) {
+          turn = 1;
+        } else {
+          turn = 0;
+        }
+        starting = 1;
+        counter(turn);
       }
       break;
 
@@ -86,14 +91,9 @@ function checkMove(player, opponent, move) {
       console.log(`${player.name} has surrendered \n${opponent.name} Won!`);
       break;
 
-    case "null":
-      break;
-
     default:
       console.log(`${player.name} choose a wrong move!`);
-      move = prompt("Please try again! What is your move? \n 1 - Attack \n 2 - Heal \n 3 - Surrender \n Please enter the number:");
-      console.log(`${player.name} choose ${move}`);
-      checkMove(player, player2, move);
+      counter(turn);
       break;
   }
 }
