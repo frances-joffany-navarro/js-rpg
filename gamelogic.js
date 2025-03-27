3887ddf9d613f05621a17294ce0929c77e0c8d5a
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2025-03-27 01:16:08 +0100

    update vampire feature

--- gamelogic.js
+++ gamelogic.js
@@ -2,28 +2,50 @@ import { Person } from "./character.js";
 
 //Initialize Temporary Characters
 const player1 = new Person("vampire", "sword", "Frances");
-const player2 = new Person("elf", "boots", "Computer");
+const player2 = new Person("vampire", "boots", "Computer");
+
+player1.displayChar();
+player2.displayChar();
 
 player1.currenthealth = 70
 
 let turn = 0;
 let starting = 1;
+let gameOver = 0;
+do {
+  counter(turn);
+}
+while (gameOver === 0);
 
-counter(turn);
 
 function counter(turn) {
+
   let move;
 
   if (turn == 0) {
-    races(player1, player2);
     if (player1.race === "vampire") {
-      const lifeStealFromOpponent = player2.currenthealth * 0.1;
-      player2.currenthealth -= lifeStealFromOpponent;
-      player1.currenthealth += lifeStealFromOpponent;
-      checkGameOver(player1);
+      const lifeStealFromOpponent = Math.round(player2.currenthealth * 0.1);
+      const p1CurrentHealth = player1.currenthealth + lifeStealFromOpponent;
+      const p2CurrentHealth = player2.currenthealth - lifeStealFromOpponent;
+      console.log(lifeStealFromOpponent, p1CurrentHealth, p2CurrentHealth);
+      if (p1CurrentHealth > player1.maxHealth) {
+        player1.currenthealth = player1.maxHealth;
+      } else {
+        player1.currenthealth = p1CurrentHealth;
+      }
+
+      if (p2CurrentHealth <= 0) {
+        console.log("Gameover!");
+        console.log(`${player2.name} lost her life.`);
+        gameOver = 1;
+      } else {
+        player2.currenthealth = p2CurrentHealth;
+      }
     }
     console.log(`${player1.name} turn!`);
-    console.log(`Current Health: ${player1.currenthealth}`);
+    console.log(`Player 1's Current Health: ${player1.currenthealth}`);
+    console.log(`Player 2's Current Health: ${player2.currenthealth}`);
+
 
     if (starting === 1) {
       move = prompt("What is your move? \n 1 - Attack \n 2 - Heal \n 3 - Surrender \n Please enter the number:");
@@ -33,19 +55,33 @@ function counter(turn) {
     }
 
     console.log(`${player1.name} choose ${move}`);
-    races(player1, player2);
+    //races(player1, player2);
     checkMove(player1, player2, move);
-    checkGameOver(player1);
+    //gameOver = checkGameOver(player1);
   } else {
-    races(player2, player1);
+
     if (player2.race === "vampire") {
-      const lifeStealFromOpponent = player1.currenthealth * 0.1;
-      player1.currenthealth -= lifeStealFromOpponent;
-      player2.currenthealth += lifeStealFromOpponent;
-      checkGameOver(player2);
+      const lifeStealFromOpponent = Math.round(player1.currenthealth * 0.1);
+      const p1CurrentHealth = player1.currenthealth - lifeStealFromOpponent;
+      const p2CurrentHealth = player2.currenthealth + lifeStealFromOpponent;
+
+      if (p2CurrentHealth > player2.maxHealth) {
+        player1.currenthealth = player1.maxHealth;
+      } else {
+        player2.currenthealth = p2CurrentHealth;
+      }
+
+      if (p1CurrentHealth <= 0) {
+        console.log("Gameover!");
+        console.log(`${player1.name} lost her life.`);
+        gameOver = 1;
+      } else {
+        player1.currenthealth = p1CurrentHealth;
+      }
     }
     console.log(`${player2.name} turn!`);
-    console.log(`Current Health: ${player2.currenthealth}`);
+    console.log(`Player 1's Current Health: ${player1.currenthealth}`);
+    console.log(`Player 2's Current Health: ${player2.currenthealth}`);
 
     if (starting === 1) {
       move = prompt("What is your move? \n 1 - Attack \n 2 - Heal \n 3 - Surrender \n Please enter the number:");
@@ -55,9 +91,9 @@ function counter(turn) {
     }
 
     console.log(`${player2.name} choose ${move}`);
-    races(player2, player1);
+    //races(player2, player1);
     checkMove(player2, player1, move);
-    checkGameOver(player2);
+    // gameOver = checkGameOver(player2);
 
     /* if (player2.currenthealth <= 0) {
       console.log(`Game over! ${player1.name} WON!`);
@@ -67,17 +103,18 @@ function counter(turn) {
   }
 }
 
-/* function item(player, opponent) {
+function item(player, opponent) {
+  races(player, opponent);
   if (opponent.item === "boots") {
     const randomLuck = Math.floor(Math.random() * 100 + 1);
     if (randomLuck <= 1 || randomLuck >= 30) {
-      console.log(`${opponent.name} is lucky. She/He dodge your attack.`);
+      return
     } else {
       console.log(`${opponent.name} is not lucky. She/He can't dodge your attack.`);
       opponent.currenthealth -= player.damage
     }
   }
-} */
+}
 
 function races(player, opponent) {
   let damagePower = player.damage();
@@ -96,10 +133,10 @@ function races(player, opponent) {
       console.log("Final: ", damagePower);
       //player.currenthealth -= damagePower;      
       //console.log("The 30% damage was deflected to you.");
-      return { damagePower, luck: true, message: "The 30% damage was deflected to you." };
+      return { playerDamage: damagePower, opponentDamage: 0, chances: 2, message: "The 30% damage was deflected to you." };
     } else {
       console.log(`${opponent.name} is not lucky. She/He can't deflect your attack.`);
-      return { playerDamage: 0, opponentDamage: damagePower, message: "You're opponent take the hit." };
+      return { playerDamage: 0, opponentDamage: damagePower, chances: 1, message: "You're opponent take the hit." };
     }
   }
 }
@@ -112,8 +149,13 @@ function checkMove(player, opponent, move) {
   switch (move) {
     case "1":
       console.log(`${player.name} wants to attack opponent`);
-
-
+      if (turn === 0) {
+        turn = 1;
+      } else {
+        turn = 0;
+      }
+      starting = 1;
+      counter(turn);
       break;
 
     case "2":
@@ -150,6 +192,7 @@ function checkMove(player, opponent, move) {
 
     case "3":
       console.log(`${player.name} has surrendered \n${opponent.name} Won!`);
+      gameOver = 1;
       break;
 
     default:
@@ -160,11 +203,11 @@ function checkMove(player, opponent, move) {
 }
 
 function checkGameOver(player) {
-  if (currenthealth <= 0) {
+  if (player.currenthealth <= 0) {
     console.log("Gameover!");
     console.log(`${player.name} lost her life.`);
     return true;
-  }else{
+  } else {
     return false;
   }
 }
